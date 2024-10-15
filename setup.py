2e75590f5ff4727f3fbb89809ea64560685d5d59
Yazdan Salimi <56175580+YazdanSalimi@users.noreply.github.com>
2024-10-15 14:26:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cardiac-attr-cm",
+    name="cardiac-attr",
     #version="0.1.0",
     author="Yazdan Salimi",
     author_email="salimiyazdan@gmail.com",
